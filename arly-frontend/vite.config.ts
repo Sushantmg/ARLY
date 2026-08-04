41eb9742fc435e42f3a0c45459711e3938cdb7af
Sushantmg <sushantmg2004@gmail.com>
2026-08-05 00:38:00 +0545

    chore: remove /api/purchase proxy rule

--- arly-frontend/vite.config.ts
+++ arly-frontend/vite.config.ts
@@ -10,10 +10,6 @@ export default defineConfig({
         target: 'http://localhost:3000',
         changeOrigin: true,
       },
-      '/api/purchase': {
-        target: 'http://localhost:3000',
-        changeOrigin: true,
-      },
       '/api': {
         target: 'http://localhost:3002',
         changeOrigin: true,
